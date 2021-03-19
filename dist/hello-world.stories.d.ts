a6a7b958f384841f8942fd4e88a70a40a8846bb0
cancio <filipe.cancio@outlook.com>
2021-03-18 23:26:10 -0300

    agr vai

---
+++ dist/hello-world.stories.d.ts
@@ -0,0 +1,7 @@
+import React from "react";
+declare const _default: {
+    title: string;
+    component: React.FC<{}>;
+};
+export default _default;
+export declare const Default: () => React.ReactNode;
---
+++ dist/index.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var React = require('react');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+
+var HelloWorld = function () {
+    return React__default['default'].createElement("div", null, "Hello World");
+};
+
+exports.HelloWorld = HelloWorld;
+//# sourceMappingURL=index.ts.map
